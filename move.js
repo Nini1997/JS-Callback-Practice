83c78f70690ed56130c6b66f83f3666684c122df
Nini1997 <141282707+Nini1997@users.noreply.github.com>
2023-09-30 23:10:22 -0400

    Update move.js

--- move.js
+++ move.js
@@ -10,3 +10,25 @@ function move(element) {
         to: moveToCoordinates
     }
 }
+
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = 'https://github.com/Nini1997/JS-Callback-Practice/blob/main/assets/green-character/static.gif?raw=true';
+  }
+  if (direction === 'west') {
+    character.src = 'https://github.com/Nini1997/JS-Callback-Practice/blob/main/assets/green-character/west.gif?raw=true';
+  }
+  if (direction === 'north') {
+    character.src = 'https://github.com/Nini1997/JS-Callback-Practice/blob/main/assets/green-character/north.gif?raw=true';
+  }
+  if (direction === 'east') {
+    character.src = 'https://github.com/Nini1997/JS-Callback-Practice/blob/main/assets/green-character/east.gif?raw=true';
+  }
+  if (direction === 'south') {
+    character.src = 'https://github.com/Nini1997/JS-Callback-Practice/blob/main/assets/green-character/south.gif?raw=true';
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+
